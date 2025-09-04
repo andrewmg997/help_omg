b967cda562b1438953460a940b62596ab08368be
andrewmg997 <amg9664@miami.edu>
2025-09-04 10:14:54 -0400

    First commit attempt

---
+++ scripts/03_content/sea surface temp.R
@@ -0,0 +1,19 @@
+#build a simple graph
+
+#load packages
+library(ggplot2)
+library(EVR628tools)
+
+
+#load data
+data("data_heatwaves")
+
+#build a plot
+p <- ggplot(data = data_heatwaves,
+       mapping = aes(x = year, y = temp_mean,
+                     group = paste(fishery, eu_rnpa))) +
+  geom_line()
+
+#export my plot
+ggsave(plot = p,
+       filename = "results/img/my_first_plot_omg.png")
